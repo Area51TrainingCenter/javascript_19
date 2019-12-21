b07b6805a64cf662ff9c04cb383fbeafb1011d13
setseroZZ <resetos35@gmail.com>
2019-12-21 15:02:47 -0500

    update

--- Modulo02/clase02/proyecto/js/listado.js
+++ Modulo02/clase02/proyecto/js/listado.js
@@ -4,35 +4,96 @@ if(!validarEstado()){
 
 const id_usuario=localStorage.id_usuario;
 
-var url_listado="http://192.168.2.44/rest/servicio/yape/amigos/id";
-var datos=new FormData();
-datos.set("id_usuario",id_usuario);
-var parametros={
-	method:"POST",
-	body:datos
-}
-fetch(url_listado,parametros)
-.then((data)=>{return data.json()})
-.then((data)=>{
-	console.log(data)
-	armarLista(data);
-});
+cargarListado();
+function cargarListado(){
 
+	var url_listado="http://192.168.2.44/rest/servicio/yape/amigos/id";
+	var datos=new FormData();
+	datos.set("id_usuario",id_usuario);
+	var parametros={
+		method:"POST",
+		body:datos
+	}
+	fetch(url_listado,parametros)
+	.then((data)=>{return data.json()})
+	.then((data)=>{
+		console.log(data)
+		armarLista(data);
+	});
+}
+
+/*
 
 const btndeslogear=document.getElementById('deslogear');
 btndeslogear.addEventListener("click",()=>{
 deslogear();
 })
-
+*/
 
 
 
 function armarLista(data){
 	var temp=""
+	var ind=1;
 	for(item of data){
-		temp=temp+`<li>${item.nombres}-${item.telefono}</li>`
+		//temp=temp+`<li>${item.nombres}-${item.telefono}</li>`
 
-			
+		temp = temp+`<li>
+		<span class="ubicacion bg-blue">${ind}</span>
+		<span class="nombres">${item.nombres}</span>
+		<span class="telefono">${item.telefono}</span>
+		</li>`	;
+		ind++;
 	}
-	document.getElementById('listado').innerHTML=temp;
+	document.getElementById('resultado-lista').innerHTML=temp;
+
+
 }
+var modal=document.getElementById('modal');
+var agregar=document.getElementById('btn_agregar');
+agregar.addEventListener("click",()=>{
+	console.log(modal);
+	modal.classList.add("show");
+})
+
+
+var agregarUsuario=document.getElementById('agregar_usuario')
+var formAmigo=document.getElementById('registro-amigo');
+var mensajeError=document.getElementById('mensaje-error');
+agregarUsuario.addEventListener("click",()=>{
+	/*
+	creo un formdata en base al formulario
+	var datosAmigo=new FormData(formAmigo);
+	// compruebo que los valores fueron guardados con las llaves correctas
+	console.log(datosAmigo.get("nombres"))
+	console.log(datosAmigo.get("telefono"))
+	*/
+
+	var datosAmigo=new FormData(formAmigo);
+	datosAmigo.set("id_usuario",localStorage.id_usuario);
+
+	var parametrosRegistro={
+		method:"POST",
+		body:datosAmigo
+	}
+	var url_registro="http://192.168.2.44/rest/servicio/yape/amigos/registro"
+	fetch(url_registro,parametrosRegistro)
+	.then((data)=>{return data.json()})
+	.then((data)=>{
+		if(data.estado==0){
+			console.log("error")
+			mensajeError.innerText=data.detalle
+			mensajeError.classList.add("show")
+		}
+		else{
+			console.log("se grabo con extio")
+			modal.classList.remove("show");
+			cargarListado();
+		}
+
+	
+
+	});
+
+
+})
---
+++ Modulo02/clase02/servicios.js
@@ -0,0 +1,31 @@
+"https://sminnova.com/rest/servicio/yape/usuarios/login"
+usuario
+password
+
+"https://sminnova.com/rest/servicio/yape/amigos/id"
+id_usuario
+
+"https://sminnova.com/rest/servicio/yape/amigos/registro"
+nombres
+telefono
+id_usuario
+
+
+"https://sminnova.com/rest/servicio/yape/pago/registro"
+nombres
+telefono
+id_usuario
+monto
+
+
+"https://sminnova.com/rest/servicio/yape/pago/historico"
+id_usuario
+
+
+
+narda 123
+diego 000
+dario 789
+miguel 123
+manuel 12345
+luis 9876
